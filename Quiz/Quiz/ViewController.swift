7a7ecf66d7745613f79caa4ffa24180987c05319
Dustin Hitzing <dustinhitzing@Dustins-MacBook-Pro-3.local>
2016-03-08 19:56:28 -0500

    Added to Interface; Added to controller

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Dustin Hitzing on 3/8/16.
+//  Copyright © 2016 Dustin Hitzing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    @IBAction func showNextQuestion(sender: AnyObject){
+        
+    }
+    
+    @IBAction func showAnswer(sender: AnyObject){
+        
+    }
+    
+}
+
