5106285c7a635b6430ee8f554e3a055e96e26db7
Dustin Hitzing <dustinhitzing@Dustins-MacBook-Pro-3.local>
2016-03-08 22:28:12 -0500

    added questions and answer models

--- Quiz/Quiz/ViewController.swift
+++ Quiz/Quiz/ViewController.swift
@@ -13,6 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
     
+    let questions: [String] = ["From what is cognac made?",
+                                "What is 7+7?",
+                                "What is the capital of Vermont?"]
+    
+    let answers: [String] = ["Grapes", "14", "Montpelier"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    
     @IBAction func showNextQuestion(sender: AnyObject){
         
     }
